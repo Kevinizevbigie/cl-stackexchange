d03f0665cde6015a1d90bff966d750e5b35784e4
Kevin Deb <vinn@kevinize.com>
2023-11-09 15:06:41 +0700

    added caching

--- stack.lisp
+++ stack.lisp
@@ -1,1 +1,25 @@
 (in-package :stack)
+
+
+
+;;===================================================
+;;-- General
+;;===================================================
+
+
+
+;;===================================================
+;;-- Caching
+;;===================================================
+
+(defvar *search-cache* (make-hash-table :size 30 :test #'equal))
+
+(defun clear-cache ()
+  (setf *search-cache* (make-hash-table :size 30 :test #'equal)))
+
+(defun save-to-link-cache (hashkey links)
+  (setf (gethash hashkey *search-cache*) links))
+
+;;===================================================
+;;-- Main Functions
+;;===================================================
